cb0e5fbc9541ded5dae0091f1ce039ddaf6fa8fb
TebexPL <tomebe1212@gmail.com>
2021-05-28 13:47:09 +0200

    Laboratorium 12

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { Route, Switch, Redirect } from "react-router-dom";
+import { isExpired } from "react-jwt";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Navbar from "./components/navbar";
 import NotFound from "./components/notFound";
 import LoginForm from "./components/loginForm";
-
+import SignUpForm from "./components/signUp";
 function App() {
   return (
     <div className="containter-fluid">
@@ -14,7 +15,15 @@ function App() {
         <div className="content">
           <Switch>
             <Route path="/login" component={LoginForm} />
-            <Route path="/posts" component={Posts} />
+            <Route path="/signUp" component={SignUpForm} />
+            <Route path="/posts"
+               render={props => {
+                   if (isExpired(localStorage.getItem('token'))) {
+                       return <Redirect to="/" />;
+                   }
+                   return <Posts />;
+               }}
+               />
             <Route path="/" exact component={Home} />
             <Route path="/not-found"  component={NotFound} />
             <Redirect to="/not-found" />
